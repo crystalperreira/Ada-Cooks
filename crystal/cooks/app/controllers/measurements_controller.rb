5399741ffe7b9c7b2146e4607526999d1d0a01ca
Crystal Perreira <cvperreira@gmail.com>
2014-11-09 00:17:05 -0800

    add edit measurement

--- crystal/cooks/app/controllers/measurements_controller.rb
+++ crystal/cooks/app/controllers/measurements_controller.rb
@@ -1,24 +1,44 @@
-class Measurementscontroller < ApplicationController
+class MeasurementsController < ApplicationController
 
   def index 
   end
 
   def create
+    raise
+    @measurement = Measurement.new(params.require(:measurement).permit(:percent, :ingredient_id, :recipe_id))
+    if @measurement.save
+      redirect_to recipe_path(@measurement.recipe_id)
+    else
+     render :new  
+    end
   end
 
   def new
+    @measurement = Measurement.new
   end
   
   def edit
+    @measurement = Measurement.find(params[:id])
   end
 
   def show
+    @measurement = Measurement.find(params[:id])
   end
 
   def update
+    @measurement = Measurement.find(params[:id])
+    if (@measurement.recipe.user_id==session[:user_id]) && @measurement.update(params.require(:measurement).permit(:percent))
+      redirect_to recipe_path(@measurement.recipe_id)
+    else
+      redirect to edit_measurement_path(@measurement.id)
+    end
   end
 
   def destroy
+    @measurement = Measurement.find(params[:id])
+    id_holder = @measurement.recipe_id
+    @measurement.destroy
+    redirect_to recipe_path(id_holder) 
   end
 
 end
--- crystal/cooks/app/controllers/recipes_controller.rb
+++ crystal/cooks/app/controllers/recipes_controller.rb
@@ -17,15 +17,37 @@ class RecipesController < ApplicationController
   end
 
   def edit
+    @recipe = Recipe.find(params[:id])
   end
 
   def show
+    @recipe = Recipe.find(params[:id])
+    @measurements = @recipe.measurements
+    @ingredients = @recipe.ingredients
+    @m_array = []
+    @i_array = []
+    @meas_id_array = []
+    @measurements.each do |m|
+      @m_array.push(m.percent)
+      @meas_id_array.push(m.id)
+    end
+    @ingredients.each do |i|
+      @i_array.push(i.name)
+    end
+    @combined = @m_array.zip(@i_array, @meas_id_array)
   end
 
   def update
+    @recipe = Recipe.find(params[:id])
+    if (@recipe.user_id==session[:user_id]) && @recipe.update(params.require(:recipe).permit(:title, :description, :public))
+      redirect_to recipe_path(params[:id])
+    else
+      render :edit
+    end
   end
 
   def destroy
+    #Erecipe.measurements.each do destroy each one associated
   end
 
 end
