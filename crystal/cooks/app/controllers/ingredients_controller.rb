104a916c611ac0f437c5a83e61d2ec27dfba976e
Crystal Perreira <cvperreira@gmail.com>
2014-11-09 21:34:57 -0800

    add and edit ingredients

--- crystal/cooks/app/controllers/ingredients_controller.rb
+++ crystal/cooks/app/controllers/ingredients_controller.rb
@@ -1,21 +1,36 @@
 class IngredientsController < ApplicationController
 
-  def index 
+  def index
+    @ingredients = Ingredient.all 
   end
 
   def create
+    @ingredient = Ingredient.new(params.require(:ingredient).permit(:name))
+      if @ingredient.save
+        redirect_to ingredients_path
+      else
+        render :new
+      end
   end
 
   def new
+    @ingredient = Ingredient.new
   end
 
   def edit
+    @ingredient = Ingredient.find(params[:id])
   end
 
   def show
   end
 
   def update
+    @ingredient = Ingredient.find(params[:id])
+    if @ingredient.update(params.require(:ingredient).permit(:name))
+      redirect_to ingredients_path
+    else
+      render :edit
+    end
   end
 
   def destroy
